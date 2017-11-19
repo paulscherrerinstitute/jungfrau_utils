7e7f4d78bf1426b8d672e3a9790cf445e761a99e
Leonardo Sala <leonardo.sala@psi.ch>
2017-11-19 08:46:07 +0100

    added entry points for showgain and create pedestals

--- jungfrau_utils/scripts/jungfrau_print_pixelmask.py
+++ jungfrau_utils/scripts/jungfrau_print_pixelmask.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python
-
-pixelMaskReasons = ['gain0', 'gain1', 'gain_bad', 'gain2', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA']
-def printReasons(mask):
-    reason=""
-    for i in range(len(pixelMaskReasons)):
-        if mask >> i & 1:
-            reason += pixelMaskReasons[i]
-            reason += " "
-    return reason
-
 import argparse
 import sys
 import os
@@ -17,22 +7,37 @@ import matplotlib.pyplot as plt
 import h5py
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", default="pedestal_res.h5", help="file with the pixel mask")
-args = parser.parse_args()
+pixelMaskReasons = ['gain0', 'gain1', 'gain_bad', 'gain2', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA']
 
-if not (os.path.isfile(args.f) and os.access(args.f, os.R_OK)):
-    print("File {} not found, exit".format(args.f))
-    exit()
 
-f = h5py.File(args.f,"r")
+def print_reasons(mask):
+    reason = ""
+    for i in range(len(pixelMaskReasons)):
+        if mask >> i & 1:
+            reason += pixelMaskReasons[i]
+            reason += " "
+    return reason
 
-pixelMask = f["pixelMask"]
 
-(sh_y,sh_x) = pixelMask.shape
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", default="pedestal_res.h5", help="file with the pixel mask")
+    args = parser.parse_args()
 
-for y in range(sh_y):
-    for x in range(sh_x):
-        if pixelMask[y][x] != 0:
-            print("Bad pixel (y,x) ({} {}) : {} ".format( y, x, printReasons(pixelMask[y][x])))
+    if not (os.path.isfile(args.f) and os.access(args.f, os.R_OK)):
+        print("File {} not found, exit".format(args.f))
+        exit()
 
+    f = h5py.File(args.f,"r")
+
+    pixelMask = f["pixelMask"]
+
+    (sh_y, sh_x) = pixelMask.shape
+
+    for y in range(sh_y):
+        for x in range(sh_x):
+            if pixelMask[y][x] != 0:
+                print("Bad pixel (y,x) ({} {}) : {} ".format( y, x, printReasons(pixelMask[y][x])))
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='jungfrau_utils',
-      version='0.0.1',
+      version='0.0.2',
       description='',
       author='Paul Scherrer Institute',
       license='GNU GPLv3',
