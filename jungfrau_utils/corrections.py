c105873b7ea8479ad2d77520dba5c0a6ad28ac02
Sala Leonardo <sala@saresb-cons-02.psi.ch>
2017-11-23 08:15:08 +0100

    docstring

--- jungfrau_utils/corrections.py
+++ jungfrau_utils/corrections.py
@@ -22,7 +22,7 @@ def apply_gain_pede_np(image, G=None, P=None, pixel_mask=None):
     else:
         g = np.ones(data.shape, dtype=np.float32)
     if P is not None:
-        p = ma.array(P[0], mask=m1, dtype=np.float32).filled(0) +         ma.array(P[1], mask=m2, dtype=np.float32).filled(0) +         ma.array(P[2], mask=m3, dtype=np.float32).filled(0)
+        p = ma.array(P[0], mask=m1, dtype=np.float32).filled(0) + ma.array(P[1], mask=m2, dtype=np.float32).filled(0) + ma.array(P[2], mask=m3, dtype=np.float32).filled(0)
     else:
         p = np.zeros(data.shape, dtype=np.float32)
     if pixel_mask is not None:
@@ -35,7 +35,7 @@ try:
     from numba import jit
 
     @jit(nopython=True, nogil=True, cache=True)
-    def apply_gain_pede_corrections_numba(m, n, image, G, P, mask, mask2, pede_mask, gain_mask):
+    def apply_gain_pede_corrections_numba(m, n, image, G, P, mask, mask2, pede_mask, gain_mask, inverse_gain=False):
         res = np.empty((m, n), dtype=np.float32)
         for i in range(m):
             for j in range(n):
@@ -43,14 +43,17 @@ try:
                     res[i][j] = 0
                     continue
                 gm = gain_mask[i][j]
-                if i==0 and j==0:
-                    print(gm, image[i][j], P[gm][i][j], G[gm][i][j])
+                #if i==0 and j==0:
+                #    print(gm, image[i][j], P[gm][i][j], G[gm][i][j])
                 if gm == 3:
                     gm = 2
-                res[i][j] = (image[i][j] - P[gm][i][j]) / G[gm][i][j]
+                if inverse_gain is False:
+                    res[i][j] = (image[i][j] - P[gm][i][j]) / G[gm][i][j]
+                else:
+                    res[i][j] = (image[i][j] - P[gm][i][j]) / G[gm][i][j]
         return res
 
-    def apply_gain_pede_numba(image, G=None, P=None, pixel_mask=None):
+    def apply_gain_pede_numba(image, G=None, P=None, pixel_mask=None, inverse_gain=False):
         
         mask = int('0b' + 14 * '1', 2)
         mask2 = int('0b' + 2 * '1', 2)
@@ -64,7 +67,7 @@ try:
         if pixel_mask is None:
             pixel_mask = np.zeros(image.shape, dtype=np.int)
 
-        return apply_gain_pede_corrections_numba(image.shape[0], image.shape[1], image, G, P, mask, mask2, pixel_mask, gain_mask)
+        return apply_gain_pede_corrections_numba(image.shape[0], image.shape[1], image, G, P, mask, mask2, pixel_mask, gain_mask, inverse_gain=inverse_gain)
 
     is_numba = True
 
@@ -72,70 +75,47 @@ except:
     print("Numba not available, reverting to Numpy")
 
 
-def apply_gain_pede(image, G=None, P=None, pixel_mask=None):
-    r"""A one-line summary that does not use variable names or the
-    function name.
-    Several sentences providing an extended description. Refer to
-    variables using back-ticks, e.g. `var`.
+def apply_gain_pede(image, G=None, P=None, pixel_mask=None, inverse_gain=False):
+    r"""Apply gain corrections to Jungfrau image. Gain and Pedestal corrections are
+    to be provided as a 3D array of shape (3, image.shape[0], image.shape[1]). 
+    The formula for the correction is: (image - P) / G
+
+    If Numba is available, a Numba-optimized routine is used: otherwise, a Numpy based one.
 
     Parameters
     ----------
-    var1 : array_like
-        Descr
+    image : array_like
+        2D array to be corrected
+    G : array_like
+        3D array containing gain corrections
+    P : array_like
+        3D array containing pedestal corrections
+    pixel_mask : array_like, int
+        2D array containing pixels to be masked (tagged with a one)
+    inverse_gain : bool
+        The inverse of the gain correction is provided
+
     Returns
     -------
-    type
-        Explanation of anonymous return value of type ``type``.
-    describe : type
-        Explanation of return value named `describe`.
-    out : type
-        Explanation of `out`.
-    Other Parameters
-    ----------------
-    only_seldom_used_keywords : type
-        Explanation
-    common_parameters_listed_above : type
-        Explanation
-    Raises
-    ------
-    BadException
-        Because you shouldn't have done that.
-    See Also
-    --------
-    otherfunc : relationship (optional)
-    newfunc : Relationship (optional), which could be fairly long, in which
-              case the line wraps here.
-    thirdfunc, fourthfunc, fifthfunc
+    res : NDArray
+        Corrected image
+
     Notes
     -----
-    Notes about the implementation algorithm (if needed).
-    This can have multiple paragraphs.
-    You may include some math:
-    .. math:: X(e^{j\omega } ) = x(n)e^{ - j\omega n}
-    And even use a greek symbol like :math:`omega` inline.
-    References
-    ----------
-    Cite the relevant literature, e.g. [1]_.  You may also cite these
-    references in the notes section above.
-    .. [1] O. McNoleg, "The integration of GIS, remote sensing,
-       expert systems and adaptive co-kriging for environmental habitat
-       modelling of the Highland Haggis using object-oriented, fuzzy-logic
-       and neural-network techniques," Computers & Geosciences, vol. 22,
-       pp. 585-588, 1996.
-    Examples
-    --------
-    These are written in doctest format, and should illustrate how to
-    use the function.
-    >>> a = [1, 2, 3]
-    >>> print [x + 3 for x in a]
-    [4, 5, 6]
-    >>> print "a\n\nb"
-    a
-    b
+    Performances for correcting a random image as of 2017-11-23, shape [1500, 1000]
+
+    Numpy
+    60 ms ± 72.7 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
+
+    Numba
+    6.23 ms ± 7.22 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
+
+    Numba plus inverse
+    6.15 ms ± 42.2 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
     """
     if is_numba:
-        return apply_gain_pede_numba(image, G=G, P=P, pixel_mask=pixel_mask)
-    return apply_gain_pede_np(image, G=G, P=P, pixel_mask=pixel_mask)
+        return apply_gain_pede_numba(image, G=G, P=P, pixel_mask=pixel_mask, inverse_gain=inverse_gain)
+    return apply_gain_pede_np(image, G=G, P=P, pixel_mask=pixel_mask, inverse_gain=inverse_gain)
 
 
 def test():
@@ -146,13 +126,20 @@ def test():
 
     t_i = time()
     res1 = apply_gain_pede_np(data, gain, pede)
-    print(time() - t_i)
+    print("NP", time() - t_i)
     t_i = time()
     res2 = apply_gain_pede_numba(data, gain, pede)    
-    print(time() - t_i)
+    print("Numba", time() - t_i)
     t_i = time()
-    res2 = apply_gain_pede(data, gain, pede)    
-    print(time() - t_i)
+    res2 = apply_gain_pede_numba(data, gain, pede)    
+    print("Numba", time() - t_i)
+    t_i = time()
+    gain2 = 1. / gain
+    res2 = apply_gain_pede(data, gain2, pede, inverse_gain=True)    
+    print("Numba inverse", time() - t_i)
+    t_i = time()
+    res2 = apply_gain_pede(data, gain2, pede, inverse_gain=True)    
+    print("Numba inverse", time() - t_i)
     #print((res1 - res2 < 0.01).all())
     #print(res1[0:2, 0:2], res2[0:2, 0:2])
 
