336599846747768f0bc0032a2e034bdfe2a8cfba
Leonardo Sala <leonardo.sala@psi.ch>
2017-11-24 10:09:26 +0100

    added plot utils

--- jungfrau_utils/plot/__init__.py
+++ jungfrau_utils/plot/__init__.py
@@ -1,1 +1,8 @@
-from .utilities import *
+import sys
+
+
+try:
+    from .utilities import *
+except:
+    print("[WARNING] cannot import plot utils, please see the below message")
+    print(sys.exc_info[1])
