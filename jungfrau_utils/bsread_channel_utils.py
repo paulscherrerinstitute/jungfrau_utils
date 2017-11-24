7e586681def9e6a14adb47711cba44a1f6ee395f
babic_a <andrej.babic@psi.ch>
2017-11-24 15:16:26 +0100

    File for loading the default channels

---
+++ jungfrau_utils/bsread_channel_utils.py
@@ -0,0 +1,11 @@
+import json
+
+
+def load_default_channel_list(filename="/sf/bernina/config/com/channel_lists/default_channel_list.json"):
+
+        with open() as input_file:
+            channels_config = json.load(input_file)
+
+        channels = channels_config["channels"]
+        return channels
+        
