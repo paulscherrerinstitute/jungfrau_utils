735f8810578cc68633bd419dea15da0bf4dc7920
Leonardo Sala <leonardo.sala@psi.ch>
2017-11-27 19:14:16 +0100

    added guid

--- jungfrau_utils/scripts/jungfrau_run_pedestals.py
+++ jungfrau_utils/scripts/jungfrau_run_pedestals.py
@@ -37,7 +37,7 @@ def main():
     print("Resetting gain bits on Jungfrau")
     reset_bits(client)
 
-    writer_config = {"output_file": args.directory + "/" + args.filename, "process_uid": args.uid, "dataset_name": "jungfrau/data", "disable_processing": False}
+    writer_config = {"output_file": args.directory + "/" + args.filename, "process_uid": args.uid, "process_gid": args.uid, "dataset_name": "jungfrau/data", "disable_processing": False}
     print(writer_config)
     if args.trigger == 0:
         detector_config = {"period": args.period, "exptime": args.exptime, "frames": args.numberFrames}
