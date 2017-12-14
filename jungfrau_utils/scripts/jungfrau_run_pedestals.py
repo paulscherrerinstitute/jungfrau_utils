9ef19f9dadb4194673d2a292065924b48f34a4f4
Leonardo Sala <leonardo.sala@psi.ch>
2017-12-14 15:27:27 +0100

    added reset on CTRL-C

--- jungfrau_utils/scripts/jungfrau_run_pedestals.py
+++ jungfrau_utils/scripts/jungfrau_run_pedestals.py
@@ -30,6 +30,8 @@ def main():
     parser.add_argument("--numberFrames", default=10000, help="Integration time (default 10000)", type=int)
     parser.add_argument("--trigger", default=1, help="run with the trigger, PERIOD will be ignored in this case(default - 1(yes))", type=int)
     parser.add_argument("--analyze", default=False, help="Run the pedestal analysis (default False)", action="store_true")
+    parser.add_argument("--nBadModules", default=0, help="Number of bad modules in the detector. Makes sense only together with --analyse (default 0)", 
+                        action="store", type=int)
     args = parser.parse_args()
 
     api_address = args.api
@@ -47,44 +49,50 @@ def main():
 
     bsread_config = {'output_file': "/dev/null", 'process_uid': args.uid, 'process_gid': args.uid, 'channels': []}
 
-    client.reset()
-    client.set_config(writer_config=writer_config, backend_config=backend_config, detector_config=detector_config, bsread_config=bsread_config)
-    print(client.get_config())
+    try:
+        client.reset()
+        client.set_config(writer_config=writer_config, backend_config=backend_config, detector_config=detector_config, bsread_config=bsread_config)
+        print(client.get_config())
 
-    sleepTime = args.numberFrames * args.period / 5
+        sleepTime = args.numberFrames * args.period / 5
 
-    print("Resetting gain bits on Jungfrau")
-    reset_bits(client)
+        print("Resetting gain bits on Jungfrau")
+        reset_bits(client)
 
-    print(client.set_detector_value("setbit", "0x5d 0"))
-    sleep(5) # for the moment there is a delay to make sure detectory is in the highG0 mode
-    print("Taking data at HG0")
-    client.start()
+        print(client.set_detector_value("setbit", "0x5d 0"))
+        sleep(5) # for the moment there is a delay to make sure detectory is in the highG0 mode
+        print("Taking data at HG0")
+        client.start()
 
-    #subprocess.check_call(["caput", "SIN-TIMAST-TMA:Evt-24-Ena-Sel", "1"])
-    sleep(sleepTime)
+        #subprocess.check_call(["caput", "SIN-TIMAST-TMA:Evt-24-Ena-Sel", "1"])
+        sleep(sleepTime)
 
-    print(client.set_detector_value("clearbit", "0x5d 0"))
-    print("Taking data at G0")
-    sleep(sleepTime)
+        print(client.set_detector_value("clearbit", "0x5d 0"))
+        print("Taking data at G0")
+        sleep(sleepTime)
 
-    print(client.set_detector_value("setbit", "0x5d 12"))
-    print("Taking data at G1")
-    sleep(sleepTime)
+        print(client.set_detector_value("setbit", "0x5d 12"))
+        print("Taking data at G1")
+        sleep(sleepTime)
 
-    print(client.set_detector_value("setbit", "0x5d 13"))
-    print("Taking data at G2")
-    sleep(2 * sleepTime)
+        print(client.set_detector_value("setbit", "0x5d 13"))
+        print("Taking data at G2")
+        sleep(2 * sleepTime)
 
-    #subprocess.check_call(["caput", "SIN-TIMAST-TMA:Evt-24-Ena-Sel", "0"])
-    client.stop()
-    client.reset()
-    reset_bits(client)
+        #subprocess.check_call(["caput", "SIN-TIMAST-TMA:Evt-24-Ena-Sel", "0"])
+        client.stop()
+        client.reset()
+        reset_bits(client)
+    except KeyboardInterrupt:
+        print("CTRL-C caught, stopping and resetting")
+        client.stop()
+        client.reset()
+        reset_bits(client)
 
     print("Pedestal run data saved in %s" % writer_config["output_file"])
     if args.analyze:
-        print("Running pedestal analysis, output file in %s", os.path.join(args.directory.replace("raw", "res"), "JF_pedestal"))
-        subprocess.call(["jungfrau_create_pedestals", "-f", writer_config["output_file"], "-o", os.path.join(args.directory.replace("raw", "res"), "JF_pedestal"), "-v", "4"])
+        print("Running pedestal analysis, output file in %s", os.path.join(args.directory.replace("raw", "res"), ""))
+        subprocess.call(["jungfrau_create_pedestals", "-f", writer_config["output_file"], "-o", os.path.join(args.directory.replace("raw", "res"), ""), "-v", "4", "-nBadModules", args.nBadModules])
     print("Done")
 
     
