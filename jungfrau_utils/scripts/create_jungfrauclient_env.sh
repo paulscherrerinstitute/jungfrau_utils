b0d8ea5db8b1a36fd2453c7c51d38ceaef2c65f7
Leonardo Sala <leonardo.sala@psi.ch>
2018-03-02 09:54:35 +0100

    updated creation script

--- jungfrau_utils/scripts/create_jungfrauclient_env.sh
+++ jungfrau_utils/scripts/create_jungfrauclient_env.sh
@@ -7,12 +7,15 @@ fi
     
 dest=$1
 
-echo "Loading psi-python36"
-module load psi-python36
+echo "Loading psi-python36/4.4.0"
+module load psi-python36/4.4.0
+
+DIR=/sf/${dest}/anaconda/4.4.0
 
 echo "Creating jungfrau_client Conda env"
 #conda create -c paulscherrerinstitute --copy -p /sf/${dest}/config/jungfrau/envs/jungfrau_client detector_integration_api ipython setuptools h5py numpy matplotlib numba jungfrau_utils
-conda create -c paulscherrerinstitute --copy -p /sf/${dest}/data/res/p16581/jungfrau/envs/jungfrau_client detector_integration_api ipython setuptools h5py numpy matplotlib numba jungfrau_utils
+#mkdir -p /sf/${dest}/anaconda/4.4.0
+conda create -c paulscherrerinstitute --copy -p $DIR/jungfrau_utils detector_integration_api ipython setuptools h5py numpy matplotlib numba jungfrau_utils
 
-source activate jungfrau_client
-#conda install --copy -c conda-forge pyFAI
+source activate $DIR/jungfrau_client
+conda install --copy -c conda-forge pyFAI
