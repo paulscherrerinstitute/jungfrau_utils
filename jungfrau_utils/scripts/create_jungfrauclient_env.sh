376154d02ea74b47b728021769e37352212db4c6
Leonardo Sala <leonardo.sala@psi.ch>
2018-03-14 10:33:00 +0100

    added packages

--- jungfrau_utils/scripts/create_jungfrauclient_env.sh
+++ jungfrau_utils/scripts/create_jungfrauclient_env.sh
@@ -16,8 +16,8 @@ echo "Creating jungfrau_client Conda env"
 #conda create -c paulscherrerinstitute --copy -p /sf/${dest}/config/jungfrau/envs/jungfrau_client detector_integration_api ipython setuptools h5py numpy matplotlib numba jungfrau_utils
 #mkdir -p /sf/${dest}/anaconda/4.4.0
 
-conda create -p $DIR ipython setuptools h5py numpy matplotlib numba dask
+conda create -p $DIR ipython setuptools h5py numpy matplotlib numba dask colorama
 
 source activate $DIR
-conda install -c paulscherrerinstitute  detector_integration_api jungfrau_utils
+conda install -c paulscherrerinstitute  detector_integration_api jungfrau_utils pyepics pyscan cam_server
 conda install --copy -c conda-forge pyFAI
