1ae51a117a96b37a45cd0ee667ec115a67640cfb
Leonardo Sala <leonardo.sala@psi.ch>
2018-06-08 17:16:55 +0200

    added file existance check

--- jungfrau_utils/scripts/download_from_audit.py
+++ jungfrau_utils/scripts/download_from_audit.py
@@ -55,6 +55,11 @@ def write_file(audit_fname, step_name, tfrom, tto):
     for rp in zip(reqs, params):
         req, par = rp
         #par["output_file"] = "/tmp/" + par["output_file"].split("/")[-1]
+        
+        if os.path.isfile(par["output_file"]):
+            print("Will not overwrite %s" % (par["output_file"]))
+            continue
+
         req["channels"].append({'name': 'SLAAR21-LSCP1-FNS:CH3:VAL_GET'})
         req["channels"].append({'name': 'SLAAR21-LSCP1-LAS6991:CH3:2'})
         print("writing %s" % par)
