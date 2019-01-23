70f2eabafe2783580b4d0f8857dc1f75ac5b4ea1
Ivan Usov <ivan.usov@psi.ch>
2019-01-23 14:35:55 +0100

    Updating for version 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jungfrau_utils',
-    version='0.2.0',
+    version='0.2.1',
     description='',
     author='Paul Scherrer Institute',
     license='GNU GPLv3',
