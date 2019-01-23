4e3f70e402af9081cd4be16abaebd762dd1c5e8b
Ivan Usov <ivan.usov@psi.ch>
2019-01-23 13:28:56 +0100

    simplify setup scripts

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
-#!/usr/bin/env python
-
-#from distutils.core import setup
 from setuptools import setup
 
-setup(name='jungfrau_utils',
-      version='0.2.0',
-      description='',
-      author='Paul Scherrer Institute',
-      license='GNU GPLv3',
-      packages=['jungfrau_utils', 'jungfrau_utils.scripts', 'jungfrau_utils.plot'],
-      # managed by conda
-      #install_requires=[
-      #    'pyzmq',
-      #],
-     )
+setup(
+    name='jungfrau_utils',
+    version='0.2.0',
+    description='',
+    author='Paul Scherrer Institute',
+    license='GNU GPLv3',
+    packages=['jungfrau_utils', 'jungfrau_utils.scripts', 'jungfrau_utils.plot'],
+    # managed by conda
+    #install_requires=[
+    #    'pyzmq',
+    #],
+)
