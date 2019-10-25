e02be150b66b3307bae49344a44ae82d9625951e
Ivan Usov <ivan.usov@psi.ch>
2019-10-25 09:41:57 +0200

    Rename jf_file -> file_adapter

--- jungfrau_utils/__init__.py
+++ jungfrau_utils/__init__.py
@@ -4,6 +4,6 @@ from jungfrau_utils.compression import compress_dataset
 from jungfrau_utils.corrections import apply_gain_pede, apply_geometry
 from jungfrau_utils.data_handler import JFDataHandler
 from jungfrau_utils.stream_adapter import StreamAdapter
-from jungfrau_utils.jf_file import File
+from jungfrau_utils.file_adapter import File
 
 __version__ = "0.4.1"
