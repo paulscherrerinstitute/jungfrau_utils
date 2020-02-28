c014f108704635bbd542400e784284459db580f7
Ivan Usov <ivan.usov@psi.ch>
2020-02-28 14:06:36 +0100

    Remove experimental compression module

--- jungfrau_utils/__init__.py
+++ jungfrau_utils/__init__.py
@@ -1,5 +1,4 @@
 from jungfrau_utils.bsread_channel_utils import load_default_channel_list
-from jungfrau_utils.compression import compress_dataset
 from jungfrau_utils.corrections import apply_gain_pede, apply_geometry
 from jungfrau_utils.data_handler import JFDataHandler
 from jungfrau_utils.file_adapter import File
