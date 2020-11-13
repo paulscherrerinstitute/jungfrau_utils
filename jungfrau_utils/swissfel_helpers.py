dd31966fe6acc15ea9fa980605bb5f2c54221a35
Ivan Usov <ivan.usov@psi.ch>
2020-11-13 14:46:08 +0100

    Support pedestal file location of the new daq

--- jungfrau_utils/swissfel_helpers.py
+++ jungfrau_utils/swissfel_helpers.py
@@ -38,8 +38,10 @@ def locate_gain_file(file_path, verbose=True):
 def locate_pedestal_file(file_path, verbose=True):
     """Locate pedestal file in default location at swissfel.
 
-    The default processed pedestal files path for a particula p-group is
-    ``/sf/<beamline>/data/<p-group>/res/JF_pedestals/``.
+    The default pedestal file paths for a particula p-group are
+    ``/sf/<beamline>/data/<p-group>/res/JF_pedestals/`` (old daq)
+    or
+    ``/sf/<beamline>/data/<p-group>/raw/JF_pedestals/`` (new daq).
 
     Args:
         file_path (str or Path): File path of a jungfrau data file.
@@ -54,22 +56,27 @@ def locate_pedestal_file(file_path, verbose=True):
 
     detector_name = _read_detector_name(file_path)
 
-    pedestal_path = Path(*file_path.parts[:5]).joinpath("res", "JF_pedestals")
+    pedestal_paths = (
+        Path(*file_path.parts[:5]).joinpath("res", "JF_pedestals"),
+        Path(*file_path.parts[:5]).joinpath("raw", "JF_pedestals"),
+    )
 
     # find a pedestal file, which was created closest in time to the jungfrau file
     jf_file_mtime = file_path.stat().st_mtime
     closest_pedestal_file = ""
     min_mtime_diff = float("inf")
-    for entry in pedestal_path.iterdir():
-        if entry.is_file() and detector_name in entry.name:
-            time_diff = jf_file_mtime - entry.stat().st_mtime
-            if abs(time_diff) < abs(min_mtime_diff):
-                min_mtime_diff = time_diff
-                pedestal_mtime = entry.stat().st_mtime
-                closest_pedestal_file = entry
+    for pedestal_path in pedestal_paths:
+        if pedestal_path.exists():
+            for entry in pedestal_path.iterdir():
+                if entry.is_file() and f"{detector_name}.res.h5" in entry.name:
+                    time_diff = jf_file_mtime - entry.stat().st_mtime
+                    if abs(time_diff) < abs(min_mtime_diff):
+                        min_mtime_diff = time_diff
+                        pedestal_mtime = entry.stat().st_mtime
+                        closest_pedestal_file = entry
 
     if not closest_pedestal_file:
-        raise Exception(f"No pedestal file in default location: {pedestal_path}")
+        raise Exception(f"No pedestal file found in default locations: {pedestal_paths}")
 
     if verbose:
         print(f"Auto-located pedestal file: {closest_pedestal_file}")
