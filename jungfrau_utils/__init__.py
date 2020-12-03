29691195a9b6c38031353fd6c68d44111ec74ae7
Ivan Usov <ivan.usov@psi.ch>
2020-12-03 10:19:14 +0100

    Remove obsolete load_default_channel_list

--- jungfrau_utils/__init__.py
+++ jungfrau_utils/__init__.py
@@ -1,4 +1,3 @@
-from jungfrau_utils.bsread_channel_utils import load_default_channel_list
 from jungfrau_utils.corrections import apply_gain_pede, apply_geometry
 from jungfrau_utils.data_handler import JFDataHandler
 from jungfrau_utils.file_adapter import File
