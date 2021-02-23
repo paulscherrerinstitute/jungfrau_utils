3fa8420dc6437a19da9ed910fee87cddd35ffec0
Ivan Usov <ivan.usov@psi.ch>
2021-02-23 08:51:30 +0100

    Updating for version 2.0.2

--- jungfrau_utils/__init__.py
+++ jungfrau_utils/__init__.py
@@ -5,4 +5,4 @@ from jungfrau_utils.stream_adapter import StreamAdapter
 from jungfrau_utils.escape_adapter import EscapeAdapter
 from jungfrau_utils.swissfel_helpers import locate_gain_file, locate_pedestal_file
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
