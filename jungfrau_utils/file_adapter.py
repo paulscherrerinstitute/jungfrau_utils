0126f391af13703ca783ac17b1f00e65eb591fcf
Ivan Usov <ivan.usov@psi.ch>
2022-10-12 11:55:16 +0200

    Skip module_map and pixel_mask copy of the old format

--- jungfrau_utils/file_adapter.py
+++ jungfrau_utils/file_adapter.py
@@ -370,6 +370,8 @@ class File:
                     name == self._data_dset_name
                     or name.endswith("frame_index")
                     or (self.conversion and name.endswith("daq_rec"))
+                    or name == f"data/{self.detector_name}/module_map"
+                    or name == f"data/{self.detector_name}/pixel_mask"
                 ):
                     return
 
