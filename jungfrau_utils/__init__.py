e33fa96cdb66fcdf400332af138e64cc06250fdc
Ivan Usov <ivan.usov@psi.ch>
2023-09-29 15:24:54 +0200

    Don't expose swissfel_helpers on the package level

--- jungfrau_utils/__init__.py
+++ jungfrau_utils/__init__.py
@@ -3,6 +3,5 @@ from jungfrau_utils.data_handler import JFDataHandler
 from jungfrau_utils.escape_adapter import EscapeAdapter
 from jungfrau_utils.file_adapter import File
 from jungfrau_utils.stream_adapter import StreamAdapter
-from jungfrau_utils.swissfel_helpers import locate_gain_file, locate_pedestal_file
 
 __version__ = "3.14.5"
